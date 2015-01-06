87b56bb60c8fa63e5190c49a4af7cc82d4212597
Terry Bu <burea1124@gmail.com>
2015-01-06 12:47:09 -0500

    Tap gesture implemented for toggling nav plus more activity indictator for childview

--- Crunchyroll iOS Test ImageViewer/ChildViewController.m
+++ Crunchyroll iOS Test ImageViewer/ChildViewController.m
@@ -8,7 +8,11 @@
 
 #import "ChildViewController.h"
 
-@interface ChildViewController ()
+@interface ChildViewController () {
+    
+    UIActivityIndicatorView *activityIndicator;
+
+}
 
 @end
 
@@ -17,6 +21,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //add tap gesture for toggling nav
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleNavigationBar)];
+    [self.view addGestureRecognizer:tap];
+    
+    [self setUpBarButtonsForNav];
+    [self downloadAndShowImageAsynchronously];
+    
+}
+
+- (void) setUpBarButtonsForNav {
     UIBarButtonItem *leftMenuButton = [[UIBarButtonItem alloc]initWithTitle:@"Menu" style:UIBarButtonItemStylePlain target:self action:@selector(goBackToParentTableView)];
     self.navigationItem.leftBarButtonItem = leftMenuButton;
     
@@ -24,11 +38,17 @@
         UIBarButtonItem *rightBarButton = [[UIBarButtonItem alloc]initWithTitle:@"Info" style:UIBarButtonItemStylePlain target:self action:@selector(showCaption)];
         self.navigationItem.rightBarButtonItem = rightBarButton;
     }
-    
-    
+}
+
+- (void) toggleNavigationBar {
+    self.navigationController.navigationBarHidden = !self.navigationController.navigationBarHidden;
+}
+
+- (void) downloadAndShowImageAsynchronously {
+    [self showActivityIndicatorUntilImageShowComplete];
+
     if (self.image.imageURL) {
         self.imageView.image = [UIImage imageNamed:@"placeHolder"];
-        
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
             NSError *error;
             NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:self.image.imageURL] options:NSDataReadingMappedIfSafe error:&error];
@@ -51,17 +71,22 @@
             if (image) {
                 dispatch_async(dispatch_get_main_queue(), ^{
                     self.imageView.image = image;
+                    [activityIndicator stopAnimating];
                 });
             }
         });
     }
+}
 
+- (void) showActivityIndicatorUntilImageShowComplete {
+    activityIndicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityIndicator.center = CGPointMake(self.view.frame.size.width/2, self.navigationController.navigationBar.frame.origin.y + 30);
+    [activityIndicator startAnimating];
+    [self.view addSubview: activityIndicator];
 }
 
 - (void) goBackToParentTableView {
-    
     [self.navigationController popViewControllerAnimated:YES];
-    
 }
 
 - (void) showCaption {
