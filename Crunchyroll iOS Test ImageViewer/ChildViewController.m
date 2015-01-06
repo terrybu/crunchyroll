d97496f41ef642e7c983c4a36ebc244baf79a8ba
Terry Bu <burea1124@gmail.com>
2015-01-06 18:39:43 -0500

    landscape alignment working better

--- Crunchyroll iOS Test ImageViewer/ChildViewController.m
+++ Crunchyroll iOS Test ImageViewer/ChildViewController.m
@@ -96,39 +96,61 @@
 }
 
 - (void) positionImage: (UIImage *) image{
-    //Portrait Mode
+    self.scrollView.contentSize = image.size;
+
+    if (UIInterfaceOrientationIsPortrait(self.view.window.rootViewController.interfaceOrientation)) {
+        [self portraitModeImageViewAlign];
+    }
+    
+    else if (UIInterfaceOrientationIsLandscape(self.view.window.rootViewController.interfaceOrientation)) {
+        [self landscapeModeImageViewAlign];
+    }
+}
+
+
+- (void) portraitModeImageViewAlign {
     CGPoint midpointOfViewFrame = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2-navbarYOffset);
     imageView.center = midpointOfViewFrame;
-    self.scrollView.contentSize = image.size;
     
-    NSLog(@"*************************");
-    NSLog(@"image size width: %f height: %f", image.size.width, image.size.height);
+    NSLog(@"*********PORTRAIT****************");
     NSLog(@"imageView x: %f y: %f width: %f height: %f", imageView.frame.origin.x, imageView.frame.origin.y, imageView.frame.size.width, imageView.frame.size.height);
-    NSLog(@"scrollView contentsize width %f, height %f", self.scrollView.contentSize.width, self.scrollView.contentSize.height);
     NSLog(@"scrollView frame width %f height %f", self.scrollView.frame.size.width, self.scrollView.frame.size.height);
     
     //when we center an image to the midpoint of the superview,
     //small enough images will be perfectly centered to the frame.
     //however, when image width is greater than the frame width,
-    //they will spill over to the left, and since contentSize starts from (0,0) towards the right,
-    //user won't be able to scroll-over to the image spillage to the left
-    //To remedy this, if we ever find an image-view spilling over, we are going to recenter it according to contentsize
-    if (image.size.width > self.view.frame.size.width && image.size.height > self.view.frame.size.height) {
-        //case of huge images with both width/height
-        imageView.center = CGPointMake(self.scrollView.contentSize.width/2, self.scrollView.contentSize.height/2);
+    //they will spill over to the left, and alignment will break
+    //To remedy this, if we ever find an image-view spilling over, we are going to recenter it according to image size
+    
+    if (imageView.frame.size.width > self.view.frame.size.width && imageView.frame.size.height > self.view.frame.size.height) {
+        //width and height both spillover
+        imageView.center = CGPointMake(imageView.frame.size.width/2, imageView.frame.size.height/2);
     }
-    else if (image.size.width > self.view.frame.size.width && image.size.height < self.view.frame.size.height) {
+    else if (imageView.frame.size.width > self.view.frame.size.width && imageView.frame.size.height < self.view.frame.size.height) {
         //case of wide images that are not that tall
-        imageView.center = CGPointMake(self.scrollView.contentSize.width/2, self.view.frame.size.height/2-navbarYOffset);
+        imageView.center = CGPointMake(imageView.frame.size.width/2, self.view.frame.size.height/2 - navbarYOffset);
+    }
+}
+
+- (void) landscapeModeImageViewAlign  {
+    CGPoint midpointOfViewFrame = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    imageView.center = midpointOfViewFrame;
+    
+    NSLog(@"**********LANDSCAPE***************");
+    NSLog(@"imageView x: %f y: %f width: %f height: %f", imageView.frame.origin.x, imageView.frame.origin.y, imageView.frame.size.width, imageView.frame.size.height);
+    NSLog(@"scrollView frame width %f height %f", self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    
+    if (imageView.frame.size.width > self.view.frame.size.width && imageView.frame.size.height > self.view.frame.size.height) {
+        imageView.center = CGPointMake(imageView.frame.size.width/2, imageView.frame.size.height/2);
     }
     
-    //Landscape Mode
-    
-    //                    if (UIInterfaceOrientationIsLandscape(self.view.window.rootViewController.interfaceOrientation)) {
-    
-
+    else if (imageView.frame.size.height > self.view.frame.size.height) {
+        imageView.center = CGPointMake(self.view.frame.size.width/2, imageView.frame.size.height/2);
+    }
     
 }
+
+
 
 - (void) goBackToParentTableView {
     [self.navigationController popViewControllerAnimated:YES];
@@ -146,20 +168,10 @@
 
 - (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
     if (UIInterfaceOrientationIsLandscape(self.view.window.rootViewController.interfaceOrientation)) {
-        
-        NSLog(@"current frame width: %f, current frame height: %f", self.view.frame.size.width, self.view.frame.size.height);
-        navbarYOffset = self.navigationController.navigationBar.frame.size.height;
-        
-        CGPoint midpointOfViewFrame = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2 + self.navigationController.navigationBar.frame.size.height);
-        imageView.center = midpointOfViewFrame;
-        if (imageView.frame.size.height + navbarYOffset >= self.view.frame.size.height) {
-            NSLog(@"change content size height to accomodate the image height");
-            self.scrollView.contentSize = CGSizeMake(self.scrollView.contentSize.width, imageView.frame.size.height + navbarYOffset);
-        }
+        [self landscapeModeImageViewAlign];
     }
     else if (UIInterfaceOrientationIsPortrait(self.view.window.rootViewController.interfaceOrientation)) {
-        CGPoint midpointOfViewFrame = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2 - navbarYOffset);
-        imageView.center = midpointOfViewFrame;
+        [self portraitModeImageViewAlign];
     }
 }
 
